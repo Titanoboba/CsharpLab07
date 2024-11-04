3c877fff235dbc0f6de5c69c292624f6419307ec
Titanoboba <nik20062005@mail.ru>
2024-11-04 12:49:57 +0300

    Добавьте файлы проекта.

---
+++ Lab07/Class1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Lab07
+{
+
+    [commentAttribute("Abstract")]
+    public abstract class Animal
+    {
+        public string Country { get; set; }
+        public bool HideFromOtherAnimals { get; set; }
+        public string Name { get; set; }
+        public eClassificationAnimal WhatAnimal { get; set; }
+
+        public Animal(string Country, bool aaaa, string name)
+        {
+            this.Country = Country;
+            this.HideFromOtherAnimals = aaaa;
+            this.Name = name;
+        }
+
+        public abstract void SayHello();
+
+        public eFavouriteFood GetFavouriteFood()
+        {
+            if (WhatAnimal == eClassificationAnimal.Herbivores)
+            {
+                return eFavouriteFood.Plants;
+            }
+            else if (WhatAnimal == eClassificationAnimal.Carnivores)
+            {
+                return eFavouriteFood.Meat;
+            }
+            else
+            {
+                return eFavouriteFood.Everything;
+            }
+        }
+
+        public void Deconstruct()
+        {
+        }
+
+        public abstract eClassificationAnimal GetClassificationAnimal();
+
+    }
+
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [commentAttribute("cow")]
+    public class Cow : Animal
+    {
+        public Cow(string country, bool hiding, string name) : base(country, hiding, name)
+        {
+
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("I am a cow");
+        }
+
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return WhatAnimal;
+        }
+
+    }
+
+    [commentAttribute("lion")]
+    public class Lion : Animal
+    {
+        public Lion(string country, bool hiding, string name) : base(country, hiding, name)
+        {
+
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("I am a lion");
+        }
+
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return WhatAnimal;
+        }
+
+    }
+
+    [commentAttribute("pig")]
+    public class Pig : Animal
+    {
+        public Pig(string country, bool hiding, string name) : base(country, hiding, name)
+        {
+
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("I am a pig");
+        }
+
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return WhatAnimal;
+        }
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class commentAttribute : Attribute
+    {
+        public string comment { get; }
+
+        public commentAttribute(string comment)
+        {
+            this.comment = comment;
+        }
+    }
+
+}
---
+++ Lab07Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Xml.Linq;
+using Lab07;
+
+class Programm
+{
+    public static void Main()
+    {
+        Lion lion = new Lion("Russia", false, "myLion");
+        Console.WriteLine(lion.Country);
+        lion.SayHello();
+
+        var root = new XElement("Classes");
+
+        foreach (var cls in Assembly.LoadFrom("Lab07ClassLibrary.dll").GetTypes().Where(t => t.IsClass))
+        {
+            var commentAttr = cls.GetCustomAttribute<commentAttribute>();
+            var comment = commentAttr?.comment ?? "defaultComment";
+
+            var classElement = new XElement("Class",
+                new XAttribute("Name", cls.Name),
+                new XAttribute("Comment", comment));
+
+            root.Add(classElement);
+        }
+
+        var xmlFilePath = "Classes.xml";
+        root.Save(xmlFilePath);
+
+        Console.WriteLine($"XML was created: {xmlFilePath}");
+    }
+}
